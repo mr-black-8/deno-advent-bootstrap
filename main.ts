3213c5b296b04b9f012823a1aea3f7eabd9b996b
Boz Black <boz.black@gmail.com>
2022-12-05 18:49:13 +1100

    add perf logging for solutions

--- main.ts
+++ main.ts
@@ -4,22 +4,18 @@ if (!day) {
   Deno.exit();
 }
 
-const input = await Deno.readTextFile(`input/${day}.input`).catch(err => {
+const input = await Deno.readTextFile(`input/${day}.input`).catch(() => {
   console.log(`Couldn't get input for day ${day}`);
-  console.log(`Check a file exists at "input/${day}.input"`)
-  console.log(err);
+  console.log(`Check the README for setup info, and make sure a file exists at "input/${day}.input"`)
   Deno.exit();
 });
 
 const inputArray = input.split('\n');
 
-console.log(`Advent of Code 2020!\nhttps://adventofcode.com/\n\n`);
+console.log(`Advent of Code 2022!\nhttps://adventofcode.com/\n\n`);
 console.log(`Running solutions for day ${day}\n`);
 
 const { solutionOne, solutionTwo } = await import(`./solutions/${day}_day.ts`);
-
-// const solutionOne = solutions[`solution${day}1`];
-// const solutionTwo = solutions[`solution${day}2`];
 
 if (!solutionOne || !solutionTwo) {
   console.log(`Solutions for day ${day} not implemented`);
@@ -27,5 +23,13 @@ if (!solutionOne || !solutionTwo) {
   Deno.exit();
 }
 
-console.log(`Day ${day}, Task 1: ${solutionOne(inputArray)}`);
-console.log(`Day ${day}, Task 2: ${solutionTwo(inputArray)}`);
+const t1 = performance.now();
+const resultOne = solutionOne(inputArray);
+const timeOne = performance.now() - t1;
+
+const t2 = performance.now();
+const resultTwo = solutionTwo(inputArray);
+const timeTwo = performance.now() - t2;
+
+console.log(`Day ${day}, Part 1: ${resultOne === 0 ? 'Not implemented' : `${resultOne} (${timeOne}ms)`}`);
+console.log(`Day ${day}, Part 2: ${resultTwo === 0 ? 'Not implemented' : `${resultTwo} (${timeTwo}ms)`}`);
